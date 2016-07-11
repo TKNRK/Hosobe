eea576897931ad2a9ff538e0bd02cb64ea42c753
takano <jugri-7441@outlook.jp>
2016-07-11 11:36:20 +0900

    ３次元版の雛形を作成。

--- python/agi.py
+++ python/agi.py
@@ -1,38 +1,56 @@
 import numpy as np
 import sympy as sp
 from scipy import optimize as opt
-from matplotlib import pyplot as plt
 from sympy.utilities.lambdify import lambdify
 from sympy import Matrix
+from tkinter import *
+import time
 
 #  initialize
+_wid = _hei = 700  # window's width and height
+wid = hei = 500  # canvas's width and height
+
+# load adjacency and multi-dimensional space
 edge = np.genfromtxt('csv/adjacency.csv', delimiter=",").astype(np.int64)
+edge_num = len(edge)
 P = np.genfromtxt('csv/mdSpace.csv', delimiter=",")
-n = len(P)
-L = np.genfromtxt('csv/eigVals.csv', delimiter=",")
-L_pos = np.array([L[i] if L[i] > 0 else 0 for i in range(n)])
-d = np.count_nonzero(L_pos)
-Ln = np.sqrt(L_pos)
-f2 = np.array(Ln[0:d])
-f2[::2] = 0
-f1 = Ln[0:d] - f2
-e1 = (f1 / np.linalg.norm(f1)).reshape(d,1)
-e2 = (f2 / np.linalg.norm(f2)).reshape(d,1)
-temp1 = e1
-temp2 = e2
+node_num, high_dim = P.shape
 
-print(P[0].shape)
-print(type(P[0]))
+dim = 2
 
-Xs = np.zeros(n)
-Ys = np.zeros(n)
+def genE():
+    L = np.sqrt(np.genfromtxt('csv/eigVals.csv', delimiter=",")[0:high_dim])
+    base = np.zeros(high_dim * dim).reshape(dim, high_dim)
+    for i in range(high_dim): base[i % dim][i] = 1
+    E = base*L
+    return E
+
+E = genE()
+
+Xs = np.zeros(node_num)
+Ys = np.zeros(node_num)
+Xs_scaled = np.zeros(node_num)
+Ys_scaled = np.zeros(node_num)
+boundingX = 0
+boundingY = 0
+
+def scale(pnt,bool):
+	if(bool): return wid*(pnt + boundingX/2)/boundingX + (_wid - wid)/2
+	else: return (hei-100)*(boundingY/2 - pnt)/boundingY + (_hei - hei)/2
+
+def unscale(pnt,bool):
+	if (bool): return boundingX * ((pnt - (_wid - wid)/2) - wid / 2) / wid
+	else: return boundingY * ((pnt - (_hei - hei)/2) - (hei - 100) / 2) / (100 - hei)
 
 def update_points():
-	for i in np.arange(n):
-		global Xs, Ys
-		p0 = P[i, 0:d]
-		Xs[i] = np.dot(p0, e1)
-		Ys[i] = np.dot(p0, e2)
+	for i in range(node_num):
+		global Xs, Ys, boundingX, boundingY
+		p0 = P[i, 0:high_dim]
+		Xs[i] = np.dot(p0, E[0]) ; Ys[i] = np.dot(p0, E[1])
+	boundingX = max([np.amax(Xs), np.amin(Xs)]) * 2
+	boundingY = max([np.amax(Ys), np.amin(Ys)]) * 2
+	for i in range(node_num):
+		Xs_scaled[i] = scale(Xs[i], True);Ys_scaled[i] = scale(Ys[i], False)
 
 update_points()
 
@@ -41,14 +59,14 @@ print("init: ready")
 # sympy
 a1,b1,c1,a2,b2,c2,t,s = sp.symbols('a1 b1 c1 a2 b2 c2 t s')   # variables
 x2_s,y2_s = sp.symbols('x2_s y2_s')  # values
-P_i = sp.MatrixSymbol('P_i', d, 1)
-E1 = sp.MatrixSymbol('E1', d, 1)
-E2 = sp.MatrixSymbol('E2', d, 1)
+P_i = sp.MatrixSymbol('P_i', high_dim, 1)
+E1 = sp.MatrixSymbol('E1', high_dim, 1)
+E2 = sp.MatrixSymbol('E2', high_dim, 1)
 var = (x2_s,y2_s,P_i,E1,E2,a1,b1,c1,a2,b2,c2,t,s)
 
-_E1 = a1*sp.Matrix(E1) + b1*sp.Matrix(E2) + c1*sp.Matrix(P_i)
-_E2 = a2*sp.Matrix(E1) + b2*sp.Matrix(E2) + c2*sp.Matrix(P_i)
-R = s*sp.Matrix(E1) + t*sp.Matrix(E2)
+_E1 = sp.Matrix(a1*E1 + b1*E2 + c1*P_i)
+_E2 = sp.Matrix(a2*E1 + b2*E2 + c2*P_i)
+R = sp.Matrix(s*E1 + t*E2)
 
 f = Matrix([
 		_E1.dot(_E1) - 1,
@@ -62,100 +80,50 @@ f = Matrix([
 		])
 
 func = sp.Matrix.norm(f)
-
 lam_f = lambdify(var, func, 'numpy')
 
 def lam(x2, y2, p, e_1, e_2):
     return lambda a1,b1,c1,a2,b2,c2,t,s: \
         lam_f(x2, y2, p, e_1, e_2, a1, b1, c1, a2, b2, c2, t, s)
 
+arr_init = np.array([1, 0, 0, 0, 1, 0, 1, 1])
 print("lambda: ready")
 
 ######## Graph Drawing ########
-identifier = ""
-arr = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+root = Tk()
+w = Canvas(root, width=_wid, height=_hei, bg='White')
+w.pack()
+circles = []
+lines = []
+r = 10
 
-class DraggableCircle:
-	def __init__(self, circle):
-		self.circle = circle
-		self.press = None
-
-	def connect(self):
-		'connect to all the events we need'
-		self.cidpress = self.circle.figure.canvas.mpl_connect(
-			'button_press_event', self.on_press)
-		self.cidrelease = self.circle.figure.canvas.mpl_connect(
-			'button_release_event', self.on_release)
-		self.cidmotion = self.circle.figure.canvas.mpl_connect(
-			'motion_notify_event', self.on_motion)
-
-	def on_press(self, event):
-		'on button press we will see if the mouse is over us and store some data'
-		if event.inaxes != self.circle.axes: return
-
-		contains, attrd = self.circle.contains(event)
-		if not contains: return
-		print('event contains', self.circle.center)
-		x0, y0 = self.circle.center
-		self.press = x0, y0, event.xdata, event.ydata
-		global identifier
-		identifier = self.circle.get_label()
-
-	def on_motion(self, event):
-		'on motion we will move the rect if the mouse is over us'
-		if self.press is None: return
-		if event.inaxes != self.circle.axes: return
-		if (self.circle.get_label() == identifier):
-			global e1, e2, temp1, temp2
-			f2 = lam(event.xdata, event.ydata, P[int(identifier)].reshape(d,1), e1, e2)
-			def g(args): return f2(*args)
-			res = opt.minimize(g, arr, method='L-BFGS-B')
-			print(res)
-			e1 = res.x[0] * temp1 + res.x[1] * temp2 + res.x[2] * P[int(identifier)].reshape(d,1)
-			e2 = res.x[3] * temp1 + res.x[4] * temp2 + res.x[5] * P[int(identifier)].reshape(d,1)
-			print(type(e1))
-			print(e1.shape)
-			temp1 = e1
-			temp2 = e2
-			update_points()
-			for i in np.arange(n):
-				nodes[i].center = (Xs[i], Ys[i])
-				nodes[i].figure.canvas.draw()
-			for i in range(len(edge)):
-				edges[i].set_xdata((Xs[edge[i,0] - 1], Xs[edge[i,1] - 1]))
-				edges[i].set_ydata((Ys[edge[i,0] - 1], Ys[edge[i,1] - 1]))
-
-	def on_release(self, event):
-		'on release we reset the press data'
-		self.press = None
-		self.circle.figure.canvas.draw()
-
-	def disconnect(self):
-		'disconnect all the stored connection ids'
-		self.circle.figure.canvas.mpl_disconnect(self.cidpress)
-		self.circle.figure.canvas.mpl_disconnect(self.cidrelease)
-		self.circle.figure.canvas.mpl_disconnect(self.cidmotion)
-
-
-gca = plt.gca()
-nodes = np.array([])
-edges = np.array([])
-
-for i in np.arange(n):
-	circle = plt.Circle((Xs[i], Ys[i]), radius=0.2, fc='y', label=str(i))
-	gca.add_patch(circle)
-	nodes = np.append(nodes, circle)
-
+# 初期描画
 for e in edge:
-	line = plt.Line2D((Xs[e[0] - 1], Xs[e[1] - 1]), (Ys[e[0] - 1], Ys[e[1] - 1]), lw=1)
-	gca.add_line(line)
-	edges = np.append(edges, line)
+	lines.append(w.create_line(Xs_scaled[e[0]-1], Ys_scaled[e[0]-1],Xs_scaled[e[1]-1], Ys_scaled[e[1]-1], fill='Black', tags='edge'))
+for i in range(node_num):
+	circles.append(w.create_oval(Xs_scaled[i] - r, Ys_scaled[i] - r, Xs_scaled[i] + r, Ys_scaled[i] + r, fill="White", tags='node'))
 
-dcs = []
-for node in nodes:
-	dc = DraggableCircle(node)
-	dc.connect()
-	dcs.append(dc)
+# 移動
+def move_node(event):
+    global E
+    temp1 = E[0]
+    temp2 = E[1]
+    x2 = unscale(event.x,True)
+    y2 = unscale(event.y,False)
+    thisID = event.widget.find_withtag(CURRENT)[0] - (edge_num+1)
+    f2 = lam(x2, y2, P[thisID].reshape(high_dim, 1), E[0].reshape(high_dim, 1),E[1].reshape(high_dim, 1))
+    def g(args): return f2(*args)
+    res = opt.minimize(g, arr_init, method='L-BFGS-B',options={'ftol':1e-3})
+    if(res.success):
+        E[0] = res.x[0] * temp1 + res.x[1] * temp2 + res.x[2] * P[thisID].reshape(high_dim, 1)
+        E[1] = res.x[3] * temp1 + res.x[4] * temp2 + res.x[5] * P[thisID].reshape(high_dim, 1)
+        update_points()
+        for i in range(node_num):
+            w.coords(circles[i], Xs_scaled[i] - r, Ys_scaled[i] - r, Xs_scaled[i] + r, Ys_scaled[i] + r)
+        for i in range(edge_num):
+            w.coords(lines[i], Xs_scaled[edge[i][0]-1], Ys_scaled[edge[i][0]-1],Xs_scaled[edge[i][1]-1], Ys_scaled[edge[i][1]-1])
 
-plt.axis('scaled')
-plt.show()
+# バインディング
+w.tag_bind('node', '<Button1-Motion>', move_node)
+
+root.mainloop()
--- python/agiTest.py
+++ python/agiTest.py
@@ -4,57 +4,54 @@ from scipy import optimize as opt
 from sympy.utilities.lambdify import lambdify
 from sympy import Matrix
 from tkinter import *
-
+import time
 
 #  initialize
-wid = 500  # view's width
-hei = 500  # view's height
+_wid = _hei = 700  # window's width and height
+wid = hei = 500  # canvas's width and height
+
+# load adjacency and multi-dimensional space
 edge = np.genfromtxt('csv/adjacency.csv', delimiter=",").astype(np.int64)
-eN = len(edge)
+edge_num = len(edge)
 P = np.genfromtxt('csv/mdSpace.csv', delimiter=",")
-n = len(P)
-L = np.genfromtxt('csv/eigVals.csv', delimiter=",")
-L_pos = np.array([L[i] if L[i] > 0 else 0 for i in range(n)])
-d = np.count_nonzero(L_pos)
-Ln = np.sqrt(L_pos)
-f2 = np.array(Ln[0:d])
-f2[::2] = 0
-f1 = Ln[0:d] - f2
-e1 = (f1 / np.linalg.norm(f1)).reshape(d,1)
-e2 = (f2 / np.linalg.norm(f2)).reshape(d,1)
-temp1 = e1
-temp2 = e2
+node_num, high_dim = P.shape
 
-print(P[0].shape)
-print(type(P[0]))
+dim = 3
 
-Xs = np.zeros(n)
-Ys = np.zeros(n)
-Xs_scaled = np.zeros(n)
-Ys_scaled = np.zeros(n)
+def genE():
+    L = np.sqrt(np.genfromtxt('csv/eigVals.csv', delimiter=",")[0:high_dim])
+    base = np.zeros(high_dim * dim).reshape(dim, high_dim)
+    for i in range(high_dim): base[i % dim][i] = 1
+    E = base*L
+    return E
+
+E = genE()
+
+Xs = np.zeros(node_num)
+Ys = np.zeros(node_num)
+Zs = np.zeros(node_num)
+Xs_scaled = np.zeros(node_num)
+Ys_scaled = np.zeros(node_num)
+Zs_scaled = np.zeros(node_num)
 boundingX = 0
 boundingY = 0
 
 def scale(pnt,bool):
-	if(bool):
-		return wid*(pnt + boundingX/2)/boundingX
-	else:
-		return (hei-100)*(boundingY/2 - pnt)/boundingY
+	if(bool): return wid*(pnt + boundingX/2)/boundingX + (_wid - wid)/2
+	else: return (hei-100)*(boundingY/2 - pnt)/boundingY + (_hei - hei)/2
 
 def unscale(pnt,bool):
-	if (bool):
-	    return boundingX * (pnt - wid / 2) / wid
-	else:
-	    return boundingY * (pnt - (hei - 100) / 2) / (100 - hei)
+	if (bool): return boundingX * ((pnt - (_wid - wid)/2) - wid / 2) / wid
+	else: return boundingY * ((pnt - (_hei - hei)/2) - (hei - 100) / 2) / (100 - hei)
 
 def update_points():
-	for i in range(n):
-		global Xs, Ys, boundingX, boundingY
-		p0 = P[i, 0:d]
-		Xs[i] = np.dot(p0, e1) ; Ys[i] = np.dot(p0, e2)
+	for i in range(node_num):
+		global Xs, Ys, Zs, boundingX, boundingY
+		p0 = P[i, 0:high_dim]
+		Xs[i] = np.dot(p0, E[0]) ; Ys[i] = np.dot(p0, E[1]) ; Zs[i] = np.dot(p0, E[2])
 	boundingX = max([np.amax(Xs), np.amin(Xs)]) * 2
 	boundingY = max([np.amax(Ys), np.amin(Ys)]) * 2
-	for i in range(n):
+	for i in range(node_num):
 		Xs_scaled[i] = scale(Xs[i], True);Ys_scaled[i] = scale(Ys[i], False)
 
 update_points()
@@ -62,79 +59,86 @@ update_points()
 print("init: ready")
 
 # sympy
-a1,b1,c1,a2,b2,c2,t,s = sp.symbols('a1 b1 c1 a2 b2 c2 t s')   # variables
-x2_s,y2_s = sp.symbols('x2_s y2_s')  # values
-P_i = sp.MatrixSymbol('P_i', d, 1)
-E1 = sp.MatrixSymbol('E1', d, 1)
-E2 = sp.MatrixSymbol('E2', d, 1)
-var = (x2_s,y2_s,P_i,E1,E2,a1,b1,c1,a2,b2,c2,t,s)
+a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3, t, s, u = sp.symbols(
+	'a1 b1 c1 d1 a2 b2 c2 d2 a3 b3 c3 d3 t s u')  # variables
+x2_s, y2_s, z2_s = sp.symbols('x2_s y2_s z2_s')  # values
+P_i = sp.MatrixSymbol('P_i', high_dim, 1)
+E1 = sp.MatrixSymbol('E1', high_dim, 1)
+E2 = sp.MatrixSymbol('E2', high_dim, 1)
+E3 = sp.MatrixSymbol('E3', high_dim, 1)
+_var = (x2_s, y2_s, z2_s, P_i, E1, E2, E3, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3, t, s, u)
 
-_E1 = a1*sp.Matrix(E1) + b1*sp.Matrix(E2) + c1*sp.Matrix(P_i)
-_E2 = a2*sp.Matrix(E1) + b2*sp.Matrix(E2) + c2*sp.Matrix(P_i)
-R = s*sp.Matrix(E1) + t*sp.Matrix(E2)
+E0 = sp.Matrix(P_i - x2_s * E1 - y2_s * E2 - z2_s * E3)
+E0 = sp.simplify(E0 / sp.Matrix.norm(E0))
 
-f = Matrix([
-		_E1.dot(_E1) - 1,
-		_E2.dot(_E2) - 1,
-		_E1.dot(_E2),
-		R.dot(R) - 1,
-		_E1.dot(R) - sp.Matrix(E1).dot(R),
-		_E2.dot(R) - sp.Matrix(E2).dot(R),
-		sp.Matrix(P_i).dot(_E1) - x2_s,
-		sp.Matrix(P_i).dot(_E2) - y2_s
-		])
+_E1 = sp.Matrix(a1 * E1 + b1 * E2 + c1 * E3 + d1 * P_i)
+_E2 = sp.Matrix(a2 * E1 + b2 * E2 + c2 * E3 + d2 * P_i)
+_E3 = sp.Matrix(a3 * E1 + b3 * E2 + c3 * E3 + d3 * P_i)
+R = sp.Matrix(t * E1 + s * E2 + u * E3)
 
-func = sp.Matrix.norm(f)
+_f = Matrix([
+	_E1.dot(_E1) - 1,
+	_E2.dot(_E2) - 1,
+	_E3.dot(_E3) - 1,
+	_E1.dot(_E2),
+	_E2.dot(_E3),
+	_E3.dot(_E1),
+	R.dot(R) - 1,
+	_E1.dot(R) - sp.Matrix(E1).dot(R),
+	_E2.dot(R) - sp.Matrix(E2).dot(R),
+	_E3.dot(R) - sp.Matrix(E3).dot(R),
+	sp.Matrix(P_i).dot(_E1) - x2_s,
+	sp.Matrix(P_i).dot(_E2) - y2_s,
+	sp.Matrix(P_i).dot(_E3) - z2_s
+])
 
-lam_f = lambdify(var, func, 'numpy')
+_func = sp.Matrix.norm(_f)
 
-def lam(x2, y2, p, e_1, e_2):
-    return lambda a1,b1,c1,a2,b2,c2,t,s: \
-        lam_f(x2, y2, p, e_1, e_2, a1, b1, c1, a2, b2, c2, t, s)
+_lam_f = lambdify(_var, _func, 'numpy')
 
-arr = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+def _lam(x2, y2, z2, p, e_1, e_2, e_3):
+	return lambda a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3, t, s, u: \
+		_lam_f(x2, y2, z2, p, e_1, e_2, e_3, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3, t, s, u)
+
+ons = np.ones(15)
+_arr = np.array(ons)
 print("lambda: ready")
 
 ######## Graph Drawing ########
-
 root = Tk()
-
-w = Canvas(root, width=wid, height=hei, bg='White')
+w = Canvas(root, width=_wid, height=_hei, bg='White')
 w.pack()
-
 circles = []
 lines = []
 r = 10
 
 # 初期描画
-
 for e in edge:
 	lines.append(w.create_line(Xs_scaled[e[0]-1], Ys_scaled[e[0]-1],Xs_scaled[e[1]-1], Ys_scaled[e[1]-1], fill='Black', tags='edge'))
-
-for i in range(n):
+for i in range(node_num):
 	circles.append(w.create_oval(Xs_scaled[i] - r, Ys_scaled[i] - r, Xs_scaled[i] + r, Ys_scaled[i] + r, fill="White", tags='node'))
 
 # 移動
 def move_node(event):
-    global e1, e2, temp1, temp2
-    x = event.x
-    y = event.y
-    x2 = unscale(int(x),True)
-    y2 = unscale(y,False)
-    thisID = event.widget.find_withtag(CURRENT)[0] - (eN+1)
-    f2 = lam(x2, y2, P[thisID].reshape(d, 1), e1, e2)
-    def g(args): return f2(*args)
-    res = opt.minimize(g, arr, method='L-BFGS-B')
-    print(res.success)
+    global E
+    temp1 = E[0]
+    temp2 = E[1]
+    temp3 = E[2]
+    x2 = unscale(event.x,True)
+    y2 = unscale(event.y,False)
+    thisID = event.widget.find_withtag(CURRENT)[0] - (edge_num+1)
+    _f2 = _lam(0, 0, 0, P[int(thisID)].reshape(high_dim, 1), E[0].reshape(high_dim, 1),E[1].reshape(high_dim, 1),E[2].reshape(high_dim, 1))
+    def _g(args):
+        return _f2(*args)
+    res = opt.minimize(_g, _arr, method='L-BFGS-B', options={'ftol': 1e-3})
     if(res.success):
-        e1 = res.x[0] * temp1 + res.x[1] * temp2 + res.x[2] * P[thisID].reshape(d, 1)
-        e2 = res.x[3] * temp1 + res.x[4] * temp2 + res.x[5] * P[thisID].reshape(d, 1)
-        temp1 = e1
-        temp2 = e2
+        E[0] = res.x[0] * temp1 + res.x[1] * temp2 + res.x[2] * temp3 + res.x[3] * P[int(thisID)]
+        E[1] = res.x[4] * temp1 + res.x[5] * temp2 + res.x[6] * temp3 + res.x[7] * P[int(thisID)]
+        E[2] = res.x[8] * temp1 + res.x[9] * temp2 + res.x[10] * temp3 + res.x[11] * P[int(thisID)]
         update_points()
-        for i in range(n):
+        for i in range(node_num):
             w.coords(circles[i], Xs_scaled[i] - r, Ys_scaled[i] - r, Xs_scaled[i] + r, Ys_scaled[i] + r)
-        for i in range(len(edge)):
+        for i in range(edge_num):
             w.coords(lines[i], Xs_scaled[edge[i][0]-1], Ys_scaled[edge[i][0]-1],Xs_scaled[edge[i][1]-1], Ys_scaled[edge[i][1]-1])
 
 # バインディング
